33b2f3b2a9d600198b4edf8d01ad67c8a21d8d85
Carin-La-Cringe <lana.del.forest@gmail.com>
2023-06-21 07:41:13 +0300

    21-06-8am

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,59 @@
 import React from 'react'
 import styles from "./Main.module.css"
+import ABOUT from "../../img/about__img.svg"
 
 const Main = () => {
   return (
     <>
       <main className={styles.Main}>
-        <div className={styles.text}>
-          Main
+
+        <div className={styles.what_it_is}>
+
+          <div className={styles.wts_card}>
+            <img src={ABOUT} alt="picture" />
+          </div>
+
+          <div className={styles.wts_card}>
+            <div className={styles.wts_text_head}>
+              Чем мы занимаемся?
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequuntur quia veniam temporibus nisi ipsum id itaque. Earum nesciunt impedit, ab ut error possimus iusto fugit quaerat ipsum illum corrupti deserunt quia cumque repudiandae quam nulla, hic fuga mollitia. Aliquid ipsam dolore ex eum provident praesentium adipisci molestias corrupti. Soluta similique cupiditate praesentium quasi odit, nostrum fugiat ad animi sit?
+          </div>
+
         </div>
+
+
+
+        <div className={styles.fast_starting}>
+          
+          2
+
+        </div>
+
+
+
+        <div className={styles.skills}>
+          3
+        </div>
+
+
+
+        <div className={styles.partners}>
+          4
+        </div>
+
+
+
+        <div className={styles.program}>
+          5
+        </div>
+
+
+
+        <div className={styles.mentors}>
+          6
+        </div>
+
       </main>
     </>
   )
